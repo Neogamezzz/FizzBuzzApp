ad043d66cec348bf696a34b1cdde74029f198418
Neogamezzz <lyosha555@outlook.com>
2023-05-29 14:27:07 +0200

    Add project files.

---
+++ FizzBuzz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FizzBuzzApp
+{
+    internal class FizzBuzz
+    {
+        internal void StartGame(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine("Your number must be divisible by 3 or 5 without a remainder.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please enter a number or press 't' for exit");
+                string val = Console.ReadLine();
+
+                if (val.ToLower() == "t")
+                {
+                    Environment.Exit(0);
+                }
+
+                if (!int.TryParse(val, out int userNumber))
+                {
+                    Console.WriteLine("Invalid value. Try again or press 't' for exit");
+                    continue;
+                }
+
+                FizzBuzz fizzBuzz = new FizzBuzz();
+                fizzBuzz.StartGame(userNumber);
+            }
+        }
+    }
+}
